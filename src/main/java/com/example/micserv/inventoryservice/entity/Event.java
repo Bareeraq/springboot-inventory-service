9751e3ba3a1bc1ebc234ce869a90395285fe75a3
Bareera Qureishi <bareera.qureishi@inadev.com>
2026-05-12 12:36:19 +0530

    uploaded inventoryservice

---
+++ src/main/java/com/example/micserv/inventoryservice/entity/Event.java
@@ -0,0 +1,32 @@
+package com.example.micserv.inventoryservice.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name= "event")
+public class Event {
+    @Id
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "total_capacity")
+    private Long total_capacity;
+
+    @Column(name = "left_capacity")
+    private Long left_capacity;
+
+    @ManyToOne
+    @JoinColumn(name = "venue_id")
+    private Venue venue;
+}
